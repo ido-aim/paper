cf49ecd0cd775cf85ded2dbc7ca16f6dad3ddf68
aimido <aim@ido.solutions>
2022-05-29 18:56:32 +0700

    update worker

--- dist/28_MAY_2022:APY Calculator/calAPY_worker.js
+++ dist/28_MAY_2022:APY Calculator/calAPY_worker.js
@@ -16,22 +16,20 @@ if( 'function' === typeof importScripts) {
     // const web3 = new Web3(); error
  
     function onMessage(e) { 
-      // do some work here 
-      let input = e.data.args; // [base,exp] i.e. [1.00000001, 26280000]
-      assert(Array.isArray(input), 'Err : input is not array');
-      let base = input[0]
-      let exp = input[1] // small number
+        // do some work here 
+        let input = e.data.args; // [base,exp] i.e. [1.00000001, 26280000]
+        assert(Array.isArray(input), 'Err : input is not array');
+        let base = input[0]
+        let exp = input[1] // small number
 
-      // long execute function
-      console.log('start calculating',input)
+        // long execute function
+        console.log('start calculating',input)
 
-      // change to reuire then execute
-      importScripts('./web3.min.js','./bignumber.min.js');
-      let web3 = new Web3();
-        //   let r = BigNumber(base).pow(365*200)
-        //   let stk = r.minus(1).multipliedBy(100);
-        //   let stkw3 = web3.toBigNumber(stk).toString(10);
-      let stkw3 = 100;
+        importScripts('./bignumber.min.js');
+        let r = BigNumber(base).pow(365*200)
+        let stk = r.minus(1).multipliedBy(100);
+        let stkw3 = stk.toFixed(10);
+        // let stkw3 = 100;
 
       console.log('got output',stkw3)
       postMessage(stkw3)
