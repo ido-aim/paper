526193bd90e41802329014eb7fd41618f846307e
aimido <aim@ido.solutions>
2022-08-03 12:40:51 +0700

    modules added

--- libs/mjs/modules.mjs
+++ libs/mjs/modules.mjs
@@ -1,12 +1,14 @@
 // js module
+// serve from GITHUB through STATICALLY
+// i.e. https://cdn.statically.io/gh/ido-aim/paper/main/libs/mjs/modules.mjs
 
 // getlink : fetch github links
 const root = "https://ido-aim.github.io/"
+
 function getLink(repo,folder,subfolder){
     // example  https://ido-aim.github.io/paper/dist/25_FEB_2022:Start/
     let url =  `${root}${repo}/${folder}/${subfolder}/`
     return url
-  
   }
 
 export {getLink}
